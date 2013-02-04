303b65c3a5d20c21041e0b02d7b181ee28a39706
Adam Bachman <adam.bachman@gmail.com>
2013-02-04 10:41:34 -0500

    restyle timer, refactor, add features, etc.

--- fragments/scripts/load_timer.js
+++ fragments/scripts/load_timer.js
@@ -1,178 +1,276 @@
-var displayTime = function (time) {
-  $('#hours').text(time.hours);
-  $('#minutes').text(time.minutes);
-  $('#seconds').text(time.seconds);
+var KEYS = {
+  ENTER:  13,
+  UP:     38,
+  DOWN:   40,
+  LEFT:   37,
+  RIGHT:  39,
+  ESCAPE: 27,
+  SPACE:  32,
+  CTRL:   17,
+  ALT:    18,
+  TAB:    9,
+  SHIFT:  16,
+  CAPS_LOCK: 20,
+  WINDOWS_KEY: 91,
+  WINDOWS_OPTION_KEY: 93,
+  BACKSPACE: 8,
+  HOME:      36,
+  END:       35,
+  INSERT:    45,
+  DELETE:    46,
+  PAGE_UP:   33,
+  PAGE_DOWN: 34,
+  NUMLOCK:   144,
+  F1:        112,
+  F2:        113,
+  F3:        114,
+  F4:        115,
+  F5:        116,
+  F6:        117,
+  F7:        118,
+  F8:        119,
+  F9:        120,
+  F10:       121,
+  F11:       122,
+  F12:       123,
+  SCROLL:    145,
+  PAUSE:     19
 }
 
-// listen for the ticking of the clock.
-chrome.extension.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.type == 'second') {
-    displayTime(message.time);
-  } else if (message.type == 'finish') {
-    $('#start_button').text('Start');
-    $('#timer-container').addClass('highlight');
-  }
-});
-
-var Grower = function (container) {
-  this.timer  = this.container = container;
-  this.button = $('#nav')
-  this.button_offsets = {
-    width:  this.button.outerWidth(true),
-    height: this.button.outerHeight(true)
-  }
-  this.update_dimensions()
-
-  // growth directions
-  this.SMALLER = 0
-  this.LARGER  = 1
-
-  // every time grow direction changes
-  this.font_growth_step = 1
-  this.growth_direction = null
-  this.direction_changes = 0
-}
-
-Grower.prototype.font_size = function () {
-  return Number(this.timer.css('font-size').split('px')[0])
-}
-
-Grower.prototype.update_dimensions = function () {
-  this.offsets = {
-    width: this.timer.width(),
-    height: this.timer.height()
-  }
-  this.display = {
-    width:  $(window).width(),
-    height: $(window).height()
-  }
-}
-
-Grower.prototype.place = function () {
-  this.update_dimensions()
-
-  this.timer.css('left', (this.display.width / 2) - (this.offsets.width / 2) + 'px')
-  this.timer.css('top',  (this.display.height / 4) - (this.offsets.height / 4) + 'px')
-
-  this.button.css('left', this.timer.css('left'));
-  // this.button.css('bottom', '10px')
-}
-
-// count how many times growth direction changes to prevent getting stuck
-Grower.prototype.choose_growth_direction = function () {
-  if (this.offsets.width > this.display.width) { // SHRINK
-    // has direction changed?
-    if (this.growth_direction === this.LARGER) {
-      this.direction_changes++
-    }
-
-    this.growth_direction = this.SMALLER
-
-    // make sure we're shrinking
-    if (this.font_growth_step > 0) this.font_growth_step *= -1
-  } else {
-    // has direction changed?
-    if (this.growth_direction === this.SMALLER) {
-      this.direction_changes++
-    }
-
-    this.growth_direction = this.LARGER
-
-    // make sure we're growing
-    if (this.font_growth_step < 0) this.font_growth_step *= -1
-  }
-}
-
-Grower.prototype.grow = function (options) {
-  options = options || {}
-  this.place()
-
-  if (Math.abs((this.display.width - 40) - this.offsets.width) > 20 && this.direction_changes < 4) {
-    this.choose_growth_direction()
-    this.timer.css('font-size', this.font_size() + this.font_growth_step + 'px')
-    this.grow()
-  } else {
-    // the right size has been reached
-    this.growth_direction = null
-    this.direction_changes = 0
-  }
-}
-
-new Grower($('#timer-container')).grow()
-
-// connect to timer app
-var bg = chrome.extension.getBackgroundPage(),
+// connect to timer app in background process
+var bg    = chrome.extension.getBackgroundPage(),
     timer = bg.app;
 
-function doTimerStart() {
+var Display = function (masterTimer) {
+  this.startButton = $('#start_button');
+  this.resetButton = $('#reset_button');
+  this.alarmCheck  = $('#alarm_check');
+  this.master      = masterTimer;
+  this.editable    = $('.editable');
+
+  console.log('check master?', masterTimer);
+
+  this.initialize();
+}
+
+Display.prototype.initialize = function () {
+  console.log('check master again?', this.master);
+
+  this.setPrimaryTime(this.master.state.displayTime);
+
+  if (this.master.settings.soundAlarm) {
+    this.alarmOn();
+  } else {
+    this.alarmOff();
+  }
+
+  // evaluate the state of the timer when the popup opens
+  if (!this.master.isRunning()) {
+    bg.clearBadge();
+    this.pauseMode();
+  } else {
+    this.runningMode();
+  }
+
+  this.bindAll();
+}
+
+Display.prototype.makeEditable = function () {
+  this.editable.attr('contenteditable', 'true')
+}
+
+Display.prototype.makeStatic = function () {
+  this.editable.attr('contenteditable', null)
+}
+
+Display.prototype.bindAll = function () {
+  var self = this;
+
+  // toggle alarm setting
+  this.alarmCheck.on('change', function () {
+    self.master.settings.soundAlarm = $(this).is(':checked');
+  });
+
+  // start / stop button
+  this.startButton.on('click', function () {
+    if (!self.master.isRunning()) {
+      self.start();
+    } else {
+      self.stop();
+    }
+  });
+
+  this.resetButton.on('click', function () {
+    if (!self.master.isRunning()) {
+      self.reset();
+    }
+  });
+
+  this.editable.
+    blur(function () {
+      // sanitize
+      var value = $(this).text()
+      if (value.length == 0 || Number(value) == 0) {
+        $(this).text('00')
+      } else if (!/^[0-9]{1,2}$/.test(value)) {
+        value = value.replace(/[^0-9]/,'').substr(0,2)
+        $(this).text(value)
+      } else {
+        // prepend single digit numbers with a zero
+        value = ((Number(value) < 10) ? '0' : '') + Number(value)
+        $(this).text(value)
+      }
+      self.makeStatic();
+    }).
+    click(function (evt) {
+      evt.stopPropagation();
+
+      // ignore clicks while timer is active
+      if (self.master.isRunning()) return;
+
+      if (!$(this).attr('contenteditable')) {
+        self.makeEditable();
+      }
+      $(this).focus();
+    }).
+    keydown(function (evt) {
+      evt.stopPropagation();
+
+      // sanitize
+      var k = evt.keyCode
+      if (k === KEYS.ESCAPE || k === KEYS.ENTER) {
+        $(this).blur()
+        evt.preventDefault()
+      } else if (k === KEYS.SPACE) {
+        evt.preventDefault()
+      }
+  });
+
+  $('document, *').on('keydown', function (evt) {
+    if (evt.which == KEYS.TAB) {
+      evt.preventDefault();
+    }
+  });
+
+  var moveOnTab = function (next, prev) {
+    return function (evt) {
+      if (evt.which == KEYS.TAB) {
+        self.makeEditable();
+
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        if (evt.shiftKey) {
+          // backwards
+          prev.focus();
+        } else {
+          // forewards
+          next.focus();
+          self.selectAll();
+        }
+
+      }
+    }
+  }
+
+  // tab and shift-tab while editing time fields
+  $('#hours').on('keydown', moveOnTab($('#minutes'), $('#seconds')));
+  $('#minutes').on('keydown', moveOnTab($('#seconds'), $('#hours')));
+  $('#seconds').on('keydown', moveOnTab($('#hours'), $('#minutes')));
+
+  $('#main-popup').on('click', function () {
+    $('.editable').blur();
+  });
+
+  $(document).on('keyup', function (evt) {
+    if (evt.which == KEYS.SPACE) {
+      if (self.master.isRunning()) {
+        self.stop();
+      } else {
+        self.start();
+      }
+    }
+  });
+}
+
+Display.prototype.start = function () {
   var hours   = Number($('#hours').text()),
       minutes = Number($('#minutes').text()),
       seconds = Number($('#seconds').text())
 
   if (hours + minutes + seconds > 0) {
-    $('#timer-container').removeClass('highlight')
-    $(this).text('Stop')
+    this.runningMode();
 
-    timer.setTime({
+    // set master timer and start counting down
+    this.master.setTime({
       hours: hours,
       minutes: minutes,
       seconds: seconds
     })
 
-    timer.start()
+    this.master.start()
   }
 }
 
-function doTimerStop() {
-  $(this).text('Start');
-  timer.stop();
+Display.prototype.reset = function () {
+  this.master.reset();
 }
 
-// start / stop button
-$('#start_button').on('click', function () {
-  if (!timer.isRunning()) {
-    doTimerStart.call(this);
-  } else {
-    doTimerStop.call(this);
+Display.prototype.stop = function () {
+  this.pauseMode();
+  this.master.stop();
+}
+
+Display.prototype.setPrimaryTime = function (time) {
+  $('#hours').text(time.hours);
+  $('#minutes').text(time.minutes);
+  $('#seconds').text(time.seconds);
+}
+
+Display.prototype.runningMode = function () {
+  this.startButton.
+    text('stop').
+    addClass('btn-danger').
+    removeClass('btn-success');
+
+  this.resetButton.
+    addClass('disabled');
+}
+
+Display.prototype.pauseMode = function () {
+  this.startButton.text('start').
+    removeClass('btn-danger').
+    addClass('btn-success');
+
+  this.resetButton.
+    removeClass('disabled');
+}
+
+Display.prototype.alarmOn = function () { this.alarmCheck.prop('checked', true); }
+
+Display.prototype.alarmOff = function () { this.alarmCheck.prop('checked', false); }
+
+Display.prototype.selectAll = function () {
+  document.execCommand('selectAll',false,null);
+}
+
+////
+//// ON LOAD
+////
+
+var display = new Display(timer);
+
+$('.editable').on('click', function () {
+  display.selectAll();
+});
+
+// listen for the ticking of the clock while the popup is open
+chrome.extension.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.type == 'second') {
+    display.setPrimaryTime(message.time);
+  } else if (message.type == 'finish') {
+    display.pauseMode();
   }
 });
 
-displayTime(timer.state.displayTime);
-
-var clock_has_focus;
-$('.editable').
-  attr('contenteditable', 'true').
-  focus(function () { clock_has_focus = true; }).
-  blur(function () {
-    // sanitize
-    var value = $(this).text()
-    if (value.length == 0 || Number(value) == 0) {
-      $(this).text('00')
-    } else if (!/^[0-9]{1,2}$/.test(value)) {
-      value = value.replace(/[^0-9]/,'').substr(0,2)
-      $(this).text(value)
-    } else {
-      // prepend single digit numbers with a zero
-      value = ((Number(value) < 10) ? '0' : '') + Number(value)
-      $(this).text(value)
-    }
-    clock_has_focus = false
-  }).
-  keydown(function (evt) {
-    // sanitize
-    var k = evt.keyCode
-    if (k === 27 || k === 13) {
-      $(this).blur()
-      evt.preventDefault()
-    } else if (k === 32) {
-      evt.preventDefault()
-    }
-  });
-
-if (timer.settings.soundAlarm) {
-  $('#alarm_check').prop('checked', true);
-}
-
-$('#alarm_check').on('change', function () {
-  timer.settings.soundAlarm = $(this).is(':checked');
-});
+$('.editable').blur();
--- scripts/background.js
+++ scripts/background.js
@@ -9,14 +9,25 @@ var TimerState = {
   running: false,
   time: {
     hours: 0,
-    minutes: 0,
-    seconds: 15
+    minutes: 5,
+    seconds: 0
   },
   displayTime: {
     hours: '00',
-    minutes: '00',
-    seconds: '15',
+    minutes: '05',
+    seconds: '00',
+  },
+  // initial default time
+  defaultTime: {
+    hours: 0,
+    minutes: 5,
+    seconds: 0
   }
+}
+
+function clearBadge() {
+  chrome.browserAction.setBadgeText({text: ""});
+  chrome.browserAction.setBadgeBackgroundColor({color: [255, 255, 255, 0]});
 }
 
 function sound_alarm() {
@@ -56,8 +67,21 @@ var TimerApp = function () {
   });
 }
 
+TimerApp.prototype.reset = function () {
+  this.state.time = this.state.defaultTime;
+  this.update(this.state.time);
+}
+
 TimerApp.prototype.setTime = function (time) {
-  // console.log('[TimerApp#setTime] setting time to', time);
+  // if given time is different than the existing time, than time fields
+  // were updated and we should save the given values as the new default
+  // time.
+
+  if (time.hours != this.state.time.hours ||
+      time.minutes != this.state.time.minutes ||
+      time.seconds != this.state.time.seconds) {
+    this.state.defaultTime = time;
+  }
 
   this.state.time = time;
 
@@ -73,9 +97,7 @@ TimerApp.prototype.setTime = function (time) {
 }
 
 TimerApp.prototype.start = function () {
-  chrome.browserAction.setBadgeText({text: ""});
-  chrome.browserAction.setBadgeBackgroundColor({color: [255, 255, 255, 0]});
-
+  clearBadge();
   this.state.running = true;
   this.clock.start(this.state.time.hours, this.state.time.minutes, this.state.time.seconds);
 }
@@ -89,7 +111,7 @@ TimerApp.prototype.update = function (time) {
   // send time updated time to popup
   this.setTime(time);
 
-  if (this.state.time.minutes <= 0) {
+  if (this.state.time.hours <= 0 && this.state.time.minutes <= 0) {
     chrome.browserAction.setBadgeBackgroundColor({color: [51, 51, 51, 255]});
     chrome.browserAction.setBadgeText({text: this.state.time.minutes + ":" + this.state.displayTime.seconds});
   }
